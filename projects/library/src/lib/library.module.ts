6fc5bcea89a27b5c2fcb0cb03ab22686db52508b
klemenoslaj <klemenoslaj0@gmail.com>
2020-12-15 15:05:50 +0100

    try exported name function

--- projects/library/src/lib/library.module.ts
+++ projects/library/src/lib/library.module.ts
@@ -1,9 +1,13 @@
 import { NgModule } from '@angular/core';
 import { LibraryComponent } from './library.component';
 
+export function provideExample() {
+  return $localize`Translated`;
+}
+
 @NgModule({
   declarations: [LibraryComponent],
   exports: [LibraryComponent],
-  providers: [{ provide: 'example', useValue: $localize`Translated` }],
+  providers: [{ provide: 'example', useFactory: provideExample }],
 })
 export class LibraryModule {}
